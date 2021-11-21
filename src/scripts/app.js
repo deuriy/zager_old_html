826393b6ae40cea7ea2f7f20b921c1cea72c272b
Yuriy Demchenko <deuriy@yandex.ru>
2021-11-21 21:59:31 +0200

    Text block layout and swiper slider

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,21 @@
-// import { Example } from "./modules/example.js";
+import Swiper from 'swiper/bundle';
 
-// new Example();
+let thumbs = new Swiper(".Swiper_thumbs", {
+  loop: true,
+  spaceBetween: 12,
+  slidesPerView: 5,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+let gallery = new Swiper(".Swiper_gallery", {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  centeredSlides: true,
+
+  thumbs: {
+    swiper: thumbs,
+    slideThumbActiveClass: 'Swiper_thumbsSlide-active'
+  }
+});
