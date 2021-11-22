fd6318aaf37d4e7bc1a7bec91d2e3bec4f256cb9
Yuriy Demchenko <deuriy@yandex.ru>
2021-11-22 15:46:13 +0200

    Adaptive layout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,42 @@
 import Swiper from 'swiper/bundle';
 
+function slideToggle(elem) {
+  if (elem.offsetHeight < elem.scrollHeight) {
+    elem.style.maxHeight = `${elem.scrollHeight}px`;
+  } else {
+    elem.style.maxHeight = '';
+  }
+}
+
+let menuHamburger = document.querySelector('.MenuHamburger');
+let mobileNavigation = document.querySelector('.MobileNavigation');
+let mobileMainMenu = document.querySelector('.MobileNavigation .MainMenu');
+// let closeMobileNavigation = document.querySelector('.MobileNavigation_closeBtn');
+
+menuHamburger.addEventListener('click', function (e) {
+  menuHamburger.classList.toggle('MenuHamburger-active');
+  mobileNavigation.classList.toggle('MobileNavigation-opened');
+
+  document.body.style.overflow = document.documentElement.clientWidth < 768 ? 'hidden' : '';
+
+  e.preventDefault();
+});
+
+document.addEventListener('click', function (e) {
+  if (document.documentElement.clientWidth > 767) {
+    if (!mobileNavigation.contains(e.target) && !menuHamburger.contains(e.target)) {
+      menuHamburger.classList.remove('MenuHamburger-active');
+      mobileNavigation.classList.remove('MobileNavigation-opened');
+    }
+  }
+});
+
+// closeMobileNavigation.addEventListener('click', function (e) {
+//   menuHamburger.classList.remove('MenuHamburger-active');
+//   mobileNavigation.classList.remove('MobileNavigation-opened');
+//   document.body.style.overflow = '';
+// });
+
 let thumbs = new Swiper(".Swiper_thumbs", {
   loop: true,
   spaceBetween: 12,
@@ -18,4 +55,14 @@ let gallery = new Swiper(".Swiper_gallery", {
     swiper: thumbs,
     slideThumbActiveClass: 'Swiper_thumbsSlide-active'
   }
+});
+
+document.addEventListener('click', function (e) {
+  let secondaryMenuTitle = e.target.closest('.SecondaryMenu_title');
+
+  if (!secondaryMenuTitle) return;
+
+  let secondaryMenu = secondaryMenuTitle.closest('.SecondaryMenu');
+  secondaryMenu.classList.toggle('SecondaryMenu-expanded');
+  slideToggle(secondaryMenu);
 });
